4c4f9b52b352e5187c3d621edc165d4f9d84ffb2
elmanlos <146708509+elmanlos@users.noreply.github.com>
2023-12-31 14:30:25 +0700

    Add files via upload

---
+++ page.php
@@ -0,0 +1,149 @@
+<DOCTYPE HTML>
+    <HTML lang="en">
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<title>facebook</title>
+<Style>
+
+
+    .pusat{
+        width:100%;
+        height:700px;
+        background-color:lightgray;
+    }
+
+    .utara{
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    }
+
+    .img{
+        width:200px;
+        margin:-10px 70px 0px 0px;
+        position:static;
+    }
+
+    .utara h2{
+        line-height:35.0px;
+        font-size:25px;
+        align-items: center;
+    }
+
+    h2{
+        justify-content:center;
+        margin:100px 0px 0px -250px;
+    }
+
+    .isi{
+        width:440px;
+        height:420px;
+        border-radius:8px;
+        background-color:white;
+        margin:100px 0px 0px 100px;
+        position:static;
+    }
+
+    .kendali{
+      margin:20px 20px;
+    }
+
+    .kendali input{
+        width:400px;
+        height:50px;
+        margin-bottom:20px;
+        border-radius:5px;
+        border-color:none;
+    }
+
+    input:hover{
+        border-color:blue;
+    }
+
+    .pus button{
+        width:400px;
+        height:50px;
+        font-size:20px;
+        background-color:blue;
+        font-family:bold;
+        color:white;
+        margin:0px 0px 30px;
+        border-radius:8px;
+        border-color:none;
+    }
+
+    a{
+        padding:0px 120px;
+        color:blue;
+        text-decoration:none;
+    }
+
+    a:hover{
+        text-decoration:underline;
+    }
+
+    .click button{
+        width:200px;
+         height:50px;
+         margin:30px 100px;
+         font-family:bold;
+         font-size:20px;
+         color:white;
+         background-color:green;
+         border-radius:8px;
+         border-color:none;
+    }
+
+    .out a{
+        margin:20px -150px;
+        color:black;
+        position:fixed;
+        font-family:40px;
+    }
+
+    .out p{
+        padding:20px 85px;
+    }
+
+    
+
+    
+
+
+
+</style>
+        </head>
+        <body>
+            <div class="pusat">
+                <div class="utara">
+                    <img class="fb_logo _8ilh img" src="https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg" alt="Facebook">
+                    <h2>FacebookFacebook helps you connect and share<br> 
+                        with the people in your life.</h2>
+                    <div class="isi">
+                        <form action="#">
+                        <div class="kendali">
+                            <input name="Email_address_or_phone_number" type="text" placeholder="Email address or phone number">
+                        
+                            <input name="Password" type="text" placeholder="Password">
+                        <div class="pus">
+                            <button>Log in</button>
+                        </div>
+
+                            <div class="link">
+                            <a href="#">Forgotten password?</a>
+                        </div>
+                        <div class="click">
+                            <button>Create new account</button>
+                        </div>
+                        </div>
+                        </form><br>
+                        <div class="out">
+                        <a href="#">Create a page</a>
+                        <p>for a celebrity, brand or business.</p>
+                    </div>
+                    
+                    </div>
+                </div>
+            </div>
+        </body>
